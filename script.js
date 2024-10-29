e7b062771babac34b6ba5c072772f88dc87566d9
DonFranklie <donfranklie@gmail.com>
2024-10-29 23:46:45 +0300

    The easy FizzBuzz

--- script.js
+++ script.js
@@ -1,45 +1,27 @@
-// Get Vowel Count
+// Fizz Buzz
 
-function getVowelCount(sentence) {
-  let vowelCount = 0;
-  let sentenceArr = Array.from(sentence);
-
-  for (let j = 0; j < sentenceArr.length; j++) {
-    let element = sentenceArr[j];
-
-    if (element === 'a') {
-      vowelCount += 1;
-    }
-       if (element === 'e') {
-      vowelCount += 1;
-    }
-
-     if (element === 'i') {
-      vowelCount += 1;
-    }
-
-     if (element === 'o') {
-      vowelCount += 1;
-    }
-
-     if (element === 'u') {
-      vowelCount += 1;
-    }
+function fizzBuzz(number) {
+  
+  if (number % 3 === 0 && number % 5 === 0 ) {
+    return 'FizzBuzz'
   }
 
-
-  return vowelCount;
+  if (number % 3 === 0) {
+    return 'Fizz'
+  }
+  
+  if (number % 5 === 0) {
+    return 'Buzz'
+  } 
+  
+  
+  
+    return number;
+  
 }
 
-console.log(getVowelCount("hello world")); // 3
-console.log(getVowelCount("JavaScript is awesome!")); // 8
-console.log(getVowelCount("Why?")); // 0
+console.log(fizzBuzz(3)); // "Fizz"
+console.log(fizzBuzz(5)); // "Buzz"
+console.log(fizzBuzz(15)); // "FizzBuzz"
+console.log(fizzBuzz(7)); // 7
 
-/*
-
-let sentence = "Hello World";
-let sentenceArr = Array.from(sentence);
-console.log(sentenceArr);
-// console.log(sentence.includes("o"));
-
-*/
